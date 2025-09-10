435453c6e6eb05452668f1027d6a0330799f10c8
Anupam <aanupamvr@gmail.com>
2025-09-10 17:19:57 +0530

    Fix: update index.js route path to use /* instead of *

--- backend/index.js
+++ backend/index.js
@@ -30,11 +30,11 @@ app.use('/api/messages', messageRoutes);
 
 if(process.env.NODE_ENV === "production"){
   app.use(express.static(path.join(__dirname, "../frontend/dist")))
-}
 
-app.get("*" , (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+});
+}
 
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
