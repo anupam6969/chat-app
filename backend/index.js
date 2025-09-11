0434297ef22e09e2f175803b29b1be14e4574746
Anupam <aanupamvr@gmail.com>
2025-09-12 00:13:59 +0530

    fix cors for production

--- backend/index.js
+++ backend/index.js
@@ -20,8 +20,8 @@ const PORT = process.env.PORT;
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true,
+    origin: frontendURL,
+    credentials: true,
 }));
 
 app.use('/api/auth', authRoutes);
