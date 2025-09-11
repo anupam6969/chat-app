74e19ece97fb296836f2792e5de6f7c31f32ba6d
Anupam <aanupamvr@gmail.com>
2025-09-12 02:32:56 +0530

    feat: Implement dynamic CORS policy for multiple environments

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "https://chat-app-2515.onrender.com/api";
 
 export const useAuthStore = create((set, get) => ({
   authUser: false,
