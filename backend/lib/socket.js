df0c47bb2f4a8219cfc1cd66c65bcebd30698979
Anupam verma <126097614+anupam6969@users.noreply.github.com>
2025-09-20 23:50:12 +0530

    Update socket.js

--- backend/lib/socket.js
+++ backend/lib/socket.js
@@ -9,7 +9,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173", "https://chat-app-zyo8.vercel.app"],
+        origin: ["http://localhost:5173", "https://chat-app-two-olive-61.vercel.app"],
+        credentials: true,
     },
 });
 
